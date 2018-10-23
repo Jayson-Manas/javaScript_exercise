6b6c49315838b52966ff8982373845ab7211338a
Jayson Manas <jaysonmanas@gmail.com>
2018-10-23 14:11:27 +0200

    javascript execise

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// var x=5
+// console.log(x)
+// console.log('javaScript')
+// var name ='jayson'
+// console.log(name)
+// console.log('my name is '+name)
+// variable type string
+// console shows name
+//add a var with a string (concatinate)
+var name= 'sally'
+var animal='dogs'
+var y=3
+var loc='America'
+console.log('my name is '+name+' and i own '+ y+' ' + animal+' and i live in '+loc)
+name= 'nuhaa'
+animal= 'cats'
+y=5
+loc='south africa'
+console.log('my name is '+name+' and i own '+ y+' ' + animal+' and i live in '+loc)
+// variables are interchanged
+
+if (new Date().getHours() < 18) {
+    document.getElementById("demo").innerHTML = "hello world";
+}
